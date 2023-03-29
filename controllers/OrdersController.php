d14b6611766d309da52a020a94e099bebe0c8dfe
Dmitriy Agapofff <agapofff@gmail.com>
2023-03-30 01:23:20 +0300

    Update OrdersController.php

--- controllers/OrdersController.php
+++ controllers/OrdersController.php
@@ -8,6 +8,7 @@ use app\models\OrdersSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
 
 /**
  * OrdersController implements the CRUD actions for Orders model.
@@ -20,6 +21,15 @@ class OrdersController extends Controller
     public function behaviors()
     {
         return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
