e6cc384d70b3b7d1219c7207a871c9477ecae070
Dmitriy Agapofff <agapofff@gmail.com>
2023-03-30 04:32:56 +0300

    Update OrdersSearch.php

--- models/OrdersSearch.php
+++ models/OrdersSearch.php
@@ -46,6 +46,11 @@ class OrdersSearch extends Orders
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
+            'sort' => [
+                'defaultOrder' => [
+                    'id' => SORT_DESC
+                ],
+            ],
         ]);
 
         $this->load($params);
